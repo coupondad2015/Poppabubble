ab5eddc71575033adfbcb0524163130ea3fc01d2
Nitesh Kumar <hash.bitcoder@gmail.com>
2025-10-24 21:07:58 +0530

    completed base ui

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,15 +4,14 @@ import React, { useEffect, useRef, useState } from "react";
 import "./hero.scss";
 import Image from "next/image";
 
-const images = [
-  "/images/store.jpg",
-  "/images/store.jpg",
-  "/images/store.jpg",
-];
+const images = ["/images/store.jpg", "/images/store.jpg", "/images/store.jpg"];
 
 const Hero = () => {
-
-  const imagesData = ["/images/store.jpg", "/images/store.jpg", "/images/store.jpg"];
+  const imagesData = [
+    "/images/store.jpg",
+    "/images/store.jpg",
+    "/images/store.jpg",
+  ];
 
   return (
     <div id="hero">
@@ -35,35 +34,71 @@ const Hero = () => {
 
       <div className="slider-wrapper">
         <div className="slider-track">
-          {imagesData.map((image, index) => (<div key={index} className="slide">
-            <Image
-              src={image}
-              alt="Slide"
-              fill
-              quality={100}
-              priority
-              style={{ objectFit: "cover" }}
-            /></div>))}
+          {imagesData.map((image, index) => (
+            <div key={index} className="slide">
+              <Image
+                src={image}
+                alt="Slide"
+                fill
+                quality={100}
+                priority
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          ))}
         </div>
       </div>
 
       <div className="yellow-border"></div>
 
       <div className="secondary-slider-wrapper">
-            <div className="secondary-slider-track">
-              {
-                imagesData.map((image, index)=>(<div key={index} className="secondary-slide">
-                  <Image
-                    src={image}
-                    alt="Slide"
-                    fill
-                    quality={100}
-                    priority
-                  /></div>))
-              }
+        <div className="secondary-slider-track">
+          {imagesData.map((image, index) => (
+            <div key={index} className="secondary-slide">
+              <Image src={image} alt="Slide" fill quality={100} priority />
             </div>
+          ))}
+        </div>
       </div>
 
+      <div className="secondary-slider-wrapper">
+        <div className="secondary-slider-track">
+          {imagesData.map((image, index) => (
+            <div key={index} className="tertiary-marquee-slide-wrapper">
+              <div className="tertiary-marquee-slide">
+                <Image src={image} alt="Slide" fill quality={100} priority />
+              </div>
+              <span style={{display: index===imagesData.length-1?'none':'inline'}}>+</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="secondary-slider-wrapper">
+        <div className="secondary-slider-track">
+          {imagesData.map((image, index) => (
+            <div key={index} className="tertiary-marquee2-slide-wrapper">
+              <div className="tertiary-marquee2-slide">
+                <Image src={image} alt="Slide" fill quality={100} priority />
+              </div>
+              <span style={{display: index===imagesData.length-1?'none':'inline'}}>+</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <footer>
+        <div className="footer-image-wrapper">
+          <Image
+            src="/images/store.jpg"
+            alt="Poppabubble Logo"
+            fill
+            quality={100}
+            priority
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </footer>
     </div>
   );
 };
