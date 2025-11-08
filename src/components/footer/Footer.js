e0ffa2cc0703073e715502f17ddacc6c662e36fd
Nitesh Kumar <hash.bitcoder@gmail.com>
2025-11-08 22:16:29 +0530

    added agency credit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import './footer.scss';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -12,11 +13,13 @@ const Footer = () => {
             height={1080}
             width={1920}
             quality={85}
-            priority={false}
             loading="lazy"
-            sizes="100vw"
             style={{ objectFit: "cover", height: '200px', width: '100%' }}
           />
+        </div>
+
+        <div className="agency-credit">
+          <p>© Poppabubble 2025 – Powered by <Link href="https://brandtec.netlify.app" target='_blank'>BRANDTec™</Link></p>
         </div>
       </footer>
   )
